056dfef5679f5d43e8589b70ba9aca0a30bf8cfa
Diogo Gomes <diogo.gomes77@gmail.com>
2022-05-15 11:44:08 +0100

    feat(1): add makeStyles

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import { Grid, Card, Avatar, CardHeader } from '@material-ui/core';
+import { Grid, Card, Avatar, CardHeader, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '25px auto',
+    maxWidth: '95vw',
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   return (
-    <Grid container spacing={3}>
+    <Grid container className={classes.root} spacing={3}>
       <Grid item xs={12} md={4}>
         <Card>
           <CardHeader
