ab74c74ac74f5db1b0ef44f2430d4c29f521544a
Diogo Gomes <diogogomes77@users.noreply.github.com>
2022-06-11 19:39:00 +0100

    2 next pipeline (#18)
    * feat(2): add gh actions
    
    * feat(2): add tests to pipeline
    
    * feat(2): add prettier to pipeline
    
    * feat(2): add husky
    
    * feat(2): add commitlint
    
    * feat(2): add semantic-release
    
    * feat(2): fix build error
    
    * feat(2): fix build error 2
    
    * feat(2): fix build error 3
    
    * feat(2): fix build error 4
    
    * feat(2): fix build error 5
    
    * feat(2): fix build error 6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? '/mycv-next-frontend/' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
