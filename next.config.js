e4688baee9ae903dc3cf0990001cefde70238de1
Diogo Gomes <diogo.gomes77@gmail.com>
2022-06-11 18:32:44 +0100

    feat(2): fix build error 2

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const debug = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? '/mycv-next-frontend/' : '',
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 };
 
 module.exports = nextConfig;
