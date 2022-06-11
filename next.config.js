dd8f888a997d795f4f305979acd24a300203c256
Diogo Gomes <diogogomes77@users.noreply.github.com>
2022-06-11 19:38:41 +0100

    Revert "2 next pipeline (#16)" (#17)
    This reverts commit d04ca98d4ee4cf7ccbe833e441195f82deacf51e.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? '/mycv-next-frontend/' : '',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
