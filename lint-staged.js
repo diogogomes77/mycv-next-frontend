325b0899700a3ae146c4cb924a09856e98bdd310
Diogo Gomes <diogo.gomes77@gmail.com>
2022-06-11 18:06:57 +0100

    feat(2): add commitlint

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0', 'yarn tests'],
-    '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0', 'yarn test'],
+  '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
 };
